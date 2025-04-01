f58818ea43e06e6f567e8145360b6f6d251c125b
HarshIyer <git@hi05.xyz>
2025-04-01 09:51:21 +0530

    Update lander

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -137,7 +137,7 @@ export default function Home() {
                   <TabsList className="grid w-full grid-cols-2 mb-8">
                     <TabsTrigger 
                       value="courses"
-                      className="flex items-center gap-2 data-[state=active]:bg-zinc-800/50"
+                      className="hover:cursor-pointer hover:bg-zinc-800/50 flex items-center gap-2 data-[state=active]:bg-purple-500/10"
                     >
                       <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
@@ -146,7 +146,7 @@ export default function Home() {
                     </TabsTrigger>
                     <TabsTrigger 
                       value="forum"
-                      className="flex items-center gap-2 data-[state=active]:bg-zinc-800/50"
+                      className="hover:cursor-pointer hover:bg-zinc-800/50 flex items-center gap-2 data-[state=active]:bg-purple-500/10"
                     >
                       <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
